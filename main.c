1f5bc91faa2acc0debcfd27d5616a125625c9a68
MDP4 <MDP4@intech.com>
2014-09-15 11:40:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -60,13 +60,13 @@ void power_block();
 void ring_bell();
 void ring_caution();
 
-unsigned char tempo=4;
-unsigned char arr[16];
+unsigned char tempo=5,new_line=0;
+char arr[16];
 char arr_t[16];
-unsigned char num1,num2,num3,num4,num5,num6;
-unsigned char arr1[8]={0x0E, 0x11, 0x0E, 0x04, 0x1F, 0x00, 0x10, 0x1F};
-unsigned char arr2[8]={0x00, 0x1E, 0x10, 0x1E, 0x00, 0x04, 0x1F, 0x00};
-unsigned char arr3[8]={0x01, 0x13, 0x13, 0x1D, 0x01, 0x08, 0x0E, 0x00};
+char num1,num2,num3,num4,num5,num6,num7;
+char arr1[8]={0x0E, 0x11, 0x0E, 0x04, 0x1F, 0x00, 0x10, 0x1F};
+char arr2[8]={0x00, 0x1E, 0x10, 0x1E, 0x00, 0x04, 0x1F, 0x00};
+char arr3[8]={0x01, 0x13, 0x13, 0x1D, 0x01, 0x08, 0x0E, 0x00};
 int alarm=0;             //alarm=1 : 경보 on, alarm=0 : 경보 off
 int triac_time;          // 트라이악(램프 제어) 인터럽트에서 시간
 char data;               //수신 문자
@@ -87,12 +87,13 @@ void main()
         LCD_display();
     }
 }
+
 void main_init(void)
 {
-    DDRB=0xff;
+    DDRB=0xFF;
     DDRC=0x00;
-    DDRD=0xff;
-    DDRE=0x0e;
+    DDRD=0xFF;
+    DDRE=0x0E;
     DDRF=0x00;
     DDRG=0x08;
     PORTG=0x00;     //PORTG.3 = 1 -> 동작 X / PORTCG.3=0 -> 동작 O
@@ -104,14 +105,14 @@ void main_init(void)
     UBRR0H=0x00;
     UBRR0L=0x07;
     lcd_init(16);
-    lcd_puts("init_OK");
+   // lcd_puts("init_OK");
 
-    rtc_set_time(7,20,7);
+    rtc_set_time(9,15,1);
     rtc_set_date(25,8,14);
     rtc_init(0,0,0);
     lcd_gotoxy(0,0);
     lcd_gotoxy(0,0);
-    lcd_puts("init_OK2");
+   // lcd_puts("init_OK2");
 
     EICRB=0b10101010;
     EIMSK=0b00110000;
@@ -123,8 +124,9 @@ void main_init(void)
 
     SREG=0x80;
     lcd_gotoxy(0,0);
-    lcd_puts("init_OK3");
+    //lcd_puts("init_OK3");
 }
+
 void LCD_display()
 {
     sprintf(arr_t,": %u",(int)((ADC_temp/5)*1023*0.01));
@@ -135,7 +137,7 @@ void LCD_display()
     lcd_putchar(0);
     lcd_putchar(1);
     lcd_puts(arr_t);
-    lcd_putchar(0b11011111);
+    lcd_putchar(0xDF);
     lcd_putsf("C 2014");
     lcd_putchar(2);
     lcd_gotoxy(0,1);
@@ -144,25 +146,25 @@ void LCD_display()
     num3=ds1302_read(0x85);
     num4=ds1302_read(0x87);
     num5=ds1302_read(0x89);
-    num6=ds1302_read(0x8b);
+    num6=ds1302_read(0x8B);
+    num7=ds1302_read(0x8D);
     sprintf (arr,"%02d/%02d %02d:%02d:%02d",(num5&0x0f)+((num5>>4)&0x0f)*10,
             (num4&0x0f)+((num4>>4)&0x02)*10,(num3&0x0f)+((num3>>4)&0x03)*10,
             (num2&0x0f)+((num2>>4)&0x0f)*10,(num1&0x0f)+((num1>>4)&0x0f)*10);
     lcd_puts(arr);
 }
+
 void communication()
 {
     unsigned int j;
     UCSR0B=0x98;
-    delay_us(100);
-    
-
+    delay_ms(10);
     switch(data)
     {
-        case VALVE_OPEN    : for(j=0;j<60;j++){PORTB.6=1; delay_us(1050); PORTB.6=0; delay_ms(23);} break;
-        case VALVE_CLOSE   : for(j=0;j<60;j++){PORTB.6=0; delay_ms(1950); PORTB.6=1; delay_ms(23);} break;
-        case WINDOW_OPEN   : for(j=0;j<60;j++){PORTB.7=1; delay_ms(1050); PORTB.7=0; delay_ms(23);} break;
-        case WINDOW_CLOSE  : for(j=0;j<60;j++){PORTB.7=0; delay_ms(1950); PORTB.7=1; delay_ms(23);} break;
+        case VALVE_OPEN    : for(j=0;j<40;j++){PORTB.6=1; delay_us(1050); PORTB.6=0; delay_ms(23);} break;
+        case VALVE_CLOSE   : for(j=0;j<40;j++){PORTB.6=0; delay_ms(1930); PORTB.6=1; delay_ms(23);} break;
+        case WINDOW_OPEN   : for(j=0;j<40;j++){PORTB.7=1; delay_ms(1050); PORTB.7=0; delay_ms(23);} break;
+        case WINDOW_CLOSE  : for(j=0;j<40;j++){PORTB.7=0; delay_ms(1930); PORTB.7=1; delay_ms(23);} break;
         case CURTAIN_OPEN  : do{PORTC.6=0; PORTC.7=1; delay_ms(10);}while(PINE.5==0);               break;
         case CURTAIN_CLOSE : do{PORTC.6=1; PORTC.7=0; delay_ms(10);}while(PINE.6==0);               break;
         case LED1_ON       : PORTC.5=0;                                                             break;
@@ -176,11 +178,12 @@ void communication()
         case LAMP_2        : triac_time=2;                                                          break;
         case LAMP_3        : triac_time=3;                                                          break;
         case LAMP_4        : triac_time=4;                                                          break;
-        case LAMP_5        : triac_time=5;                                                          break;
+        case LAMP_5        : triac_time=5; 
     }
-    delay_us(5);
+    delay_us(10);
     UCSR0B=0x18;
 }
+
 void warning_sound()
 {
     if((alarm==0)&&(ADC_human>500))
@@ -192,9 +195,10 @@ void warning_sound()
     else if((alarm==1)&&(ADC_human>500))
         {
             PORTC.5=1;
-            ring_caution();            //경보 (침입자)
+            while(1)ring_caution();            //경보 (침입자)
         }
 }
+
 void power_block()
 {
     if((ADC_smoke>300) && ((int)((ADC_temp/5)*1023*0.01)>50))
@@ -205,6 +209,7 @@ void power_block()
     if(PINE.7==0)
         PORTG=0x00;
 }
+
 void ADC_smoke_sensor()
 {
     ADC_state=0;
@@ -212,8 +217,9 @@ void ADC_smoke_sensor()
     ADCSRA=0xcf;
     delay_ms(5);
     ADCSRA=0xc7;
-    delay_ms(2);
+    delay_ms(5);
 }
+
 void ADC_temperature()
 {
     ADC_state=1;
@@ -221,8 +227,9 @@ void ADC_temperature()
     ADCSRA=0xcf;
     delay_ms(5);
     ADCSRA=0xc7;
-    delay_ms(2);
+    delay_ms(5);
 }
+
 void ADC_human_check()
 {
     ADC_state=2;
@@ -230,8 +237,9 @@ void ADC_human_check()
     ADCSRA=0xcf;
     delay_ms(5);
     ADCSRA=0xc7;       
-    delay_ms(2);  
+    delay_ms(5);  
 }
+
 interrupt [ADC_INT] void adc_project(void)
 {
     switch(ADC_state)
@@ -240,15 +248,14 @@ interrupt [ADC_INT] void adc_project(void)
         case 1 : ADC_temp=ADCW;
         case 2 : ADC_human=ADCW;
     }
-    //ADC_temp=(int)(((ADC_temp*5)/1023)*0.01);
 }
+
 interrupt [EXT_INT4] void int_bell()
 {
-    EIMSK=0x20;
     ring_bell();                //초인종
-    delay_ms(10);
-    EIMSK=0x30;
+    delay_ms(2);
 }
+
 interrupt [EXT_INT5] void triac_bright1()
 {
     PORTB.4=1;
@@ -263,6 +270,7 @@ interrupt [EXT_INT5] void triac_bright1()
     }
     PORTB.4=0;
 }
+
 void Play_note(unsigned int sound, unsigned int note)
 {
     ICR1= sound;
@@ -271,24 +279,35 @@ void Play_note(unsigned int sound, unsigned int note)
     delay_ms(note*tempo*7);
     TCCR1B = 0x18;
 }
+
 interrupt [USART0_RXC] void a(void)
 {
-    data=UDR0;
-    delay_us(5);
+    new_line++;
+    if(new_line==9)
+    {
+      data=UDR0;
+      delay_us(5);
+      new_line=0;
+    }
+    
 }
+
 void string(char *p,char code)
 {
     char i,a;
     a=(code<<3)|0x40;
     for(i=0;i<8;i++)lcd_write_byte(a++,*p++);
 }
+
 void ring_caution()
 {
     Play_note(HE,N1);
 }
+
 void ring_bell()
 {
-
+   Play_note(ME,N4);
+   Play_note(ME,N4);
 }
 /*
 #include <mega128.h>
